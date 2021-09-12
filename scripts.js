557454fce6631d93897acb4df257af8c88272d22
Matthias Clein <m.cleinespinoza@gmail.com>
2021-09-12 02:42:44 -0300

    clasifica imágenes desde webcam

--- scripts.js
+++ scripts.js
@@ -2,12 +2,22 @@ let net;
 
 const imgEl = document.getElementById('img');
 const descEl = document.getElementById('descripcion_imagen');
+const webcamElement = document.getElementById('webcam');
 
 async function app(){
     net = await mobilenet.load();
     var result = await net.classify(imgEl);
     console.log(result);
     displayImagePrediction();
+    webcam = await tf.data.webcam(webcamElement);
+    
+    while(true){
+        const img = await webcam.capture();
+        const result = await net.classify(img);
+        document.getElementById('console').innerHTML = 'prediction:' + result[0].className + ' ' + 'probability:' + result[0].probability
+        img.dispose();
+        await tf.nextFrame();
+    }
 }
 
 imgEl.onload = async function(){
